399dfce7b346ae51cdbd184cd30fe3facff79dde
PritamDe166 <Pritamde166@gmail.com>
2025-08-04 22:58:30 +0530

    Conditional exercises

--- Conditionals.py
+++ Conditionals.py
@@ -19,7 +19,7 @@ else:
     print("The number is odd.")
     
 
-# Exercie: Compare two numbers and print the larger one, or if they are equal.
+# Exercise: Compare two numbers and print the larger one, or if they are equal.
 
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
@@ -30,3 +30,19 @@ elif num1 < num2:
     print("The 2nd number is greater than the first number")
 else:
     print("The numbers are equal")
+    
+    
+# Exercise : Take a score between 0–100 and print the grade:
+
+num = (int(input("Enter your score: ")))
+
+if num < 60:
+    print("You have Failed")
+elif num >= 60 and num < 70:
+    print("Your Grade is: D")
+elif num >= 70 and num < 80:
+    print("Ypur Grade is : C")
+elif  num >= 80 and num < 90:
+    print("Your Grade is: B")
+else:
+    print("Your Grade is: A")
