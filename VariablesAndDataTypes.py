da29a9d4ce563d3a3036d509bf925b44cd04d48c
PritamDe166 <Pritamde166@gmail.com>
2025-08-04 17:06:38 +0530

    Basic datatypes

--- VariablesAndDataTypes.py
+++ VariablesAndDataTypes.py
@@ -1,1 +1,21 @@
-print("These are the variable in python")
+
+print("Variables and Data Types Example")
+
+
+name = "John"       # String Variable 
+age = 30            # Integer Variable
+is_student = True   # Boolean Variable
+height = 5.9        # Float/Decimal Variable
+
+print("Name:", name)
+print("Age:", age)
+print ("Student:", is_student)
+print("Height:", height)
+
+
+'''
+This is an example of a multi-line comment.
+You can use multi-line comments to explain your code in detail.
+'''
+
+
