72f0e903ddeb9025c8a96950f73df27959713d27
PritamDe166 <Pritamde166@gmail.com>
2025-08-13 23:53:58 +0530

    functions practise

---
+++ Built-in_Functions_Type_Conversion.py
@@ -0,0 +1,42 @@
+# type()
+var1 = "pritam"
+var2 = 60
+
+print(type(var1))
+print(type(var2))
+
+# isInstance()
+
+print(isinstance("pritam", str))
+print(isinstance(54, str))
+print(isinstance(1.5, float))
+
+# int()
+print(int(4.5))
+print(int("42"))
+
+# float()
+print(float(4))
+print(float("43"))
+
+# str()
+print(str(4) + " apples")
+
+# bool()
+print(bool(0))
+print(bool(5))
+print(bool(1))
+print(bool(""))
+print(bool("Hello"))
+
+# list()
+print(list((1,2,3,4)))
+
+# tuple()
+print(tuple("abc"))
+
+# set()
+print(set([1, 1, 2, 3, 3]))
+
+# dict()
+print(dict([("name", "Alice"), ("age", 25)]))
