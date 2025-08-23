1f5145eeb4240b3a6029c9354e0ca2be688c4588
PritamDe166 <Pritamde166@gmail.com>
2025-08-23 18:44:11 +0530

    Commiting changes

---
+++ BuiltinFunctions_Math_Numbers.py
@@ -0,0 +1,26 @@
+# abs()
+print(abs(-7))
+print(abs(5.4))
+
+# round()
+print(round(4.56756, 2))
+print(round(8.542))
+print(round(-13.349))
+
+# pow()
+print(pow(2, 3))
+print(pow(4, 3))
+print(pow(9, 2))
+
+
+# min()
+print(min([5, 9, 2, 6, 1, 4]))
+print(min((7, 10, 3, 8)))
+
+# max()
+print(max([45, 32, 12, 87, 98]))
+print(max((32, 45, 65, 21)))
+
+# sum()
+print(sum([2, 5, 4, 7]))
+print(sum((5, 6, 3, 9)))
