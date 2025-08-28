d60fab8b75e0d7e437d2c17deadf2778ae312d51
PritamDe166 <Pritamde166@gmail.com>
2025-08-29 00:52:51 +0530

    Changes commit

--- BuiltinFunctions_Iterables.py
+++ BuiltinFunctions_Iterables.py
@@ -1,11 +1,11 @@
-# # # len() - length
-# # print(len([4, 5, 1, 3]))
+# # len() - length
+print(len([4, 5, 1, 3]))
 
-# # sorted() - Sorting
-# nums1 = [6, 5, 8, 3, 2, 9, 11, 14, 10]
-# print(sorted(nums1))
-# print(sorted(nums1, reverse=True))
-# print(nums1)
+# sorted() - Sorting
+nums1 = [6, 5, 8, 3, 2, 9, 11, 14, 10]
+print(sorted(nums1))
+print(sorted(nums1, reverse=True))
+print(nums1)
 
 # reversed() - Reverse Iterator
 nums2 = [50, 10, 40, 20]
